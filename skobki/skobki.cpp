e8cd41ce16849d3a75945b9fdf9e47975e6972cd
VityaRa <blackops2in@mail.ru>
2021-02-28 00:38:51 +0300

    proj deployed

---
+++ skobki/skobki.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool isRightStr(string strToCheck) {
+    char activeBracket = ' ';
+    string prevReverseBrackets;
+    char activeReverseBracket = ' ';
+    for (char symbol : strToCheck) {
+        if (symbol == '[') {
+            activeBracket = symbol;
+            activeReverseBracket = ']';
+            prevReverseBrackets.push_back(activeReverseBracket);
+
+        }
+        if (symbol == '{') {
+            activeBracket = symbol;
+            activeReverseBracket = '}';
+            prevReverseBrackets.push_back(activeReverseBracket);
+        }
+        if (symbol == '(') {
+            activeBracket = symbol;
+            activeReverseBracket = ')';
+            prevReverseBrackets.push_back(activeReverseBracket);
+        }
+        if (symbol == ']' || symbol == ')' || symbol == '}') {
+            if (activeReverseBracket == symbol) { 
+                activeReverseBracket = prevReverseBrackets[prevReverseBrackets.length() - 1];
+                prevReverseBrackets.pop_back();
+                continue; 
+            }
+            else return false;
+        }
+    }
+    return !(prevReverseBrackets.length());
+}
+
+int main()
+{
+    cout << isRightStr("(()");
+}
